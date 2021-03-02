64eeb7bb20a67922634d50929a81717876610116
Pharoah Campbell <pharoahcampbell@gmail.com>
2021-03-02 11:57:20 -0500

    Added spacing and the button to the nav bar

--- src/NavigationBar.tsx
+++ src/NavigationBar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import './NavigationBar.css';
 
 export class NavigationBar extends React.Component<{}, {}>
 {
@@ -8,13 +10,22 @@ export class NavigationBar extends React.Component<{}, {}>
 
       <div>
         <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">Omcoin</Navbar.Brand>
+          <div className="brandSpacing">
+            <Navbar.Brand href="#home">Omcoin</Navbar.Brand>
+          </div>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Overview</Nav.Link>
-              <Nav.Link href="#link">Features</Nav.Link>
-            </Nav>
+            <div className="PageLinks">
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Overview</Nav.Link>
+                <Nav.Link href="#link">Features</Nav.Link>
+              </Nav>
+            </div>
+            <div className = "buttonSpacing">
+              <Button variant = "primary"> Read our white paper </Button>
+            </div>
+            
           </Navbar.Collapse>
         </Navbar>
       </div>
