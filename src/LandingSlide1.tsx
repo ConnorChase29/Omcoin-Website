b6fe36fd105a3ed22d18e4da91ef407ee82f6acb
Pharoah Campbell <pharoahcampbell@gmail.com>
2021-03-09 10:45:47 -0500

    refactored landing slide 1 page

--- src/LandingSlide1.tsx
+++ src/LandingSlide1.tsx
@@ -11,37 +11,29 @@ export class LandingSlide1 extends React.Component<{}, {}>
     render() {
         return (
             <div className="Landing1">
-                <div>
-                    <img id="omcoinLogo" src={Logo} alt="Omcoin Logo" />
+                <div className = "omcoinLogoDiv">
+                    <img src={Logo} alt="Omcoin Logo" />
                 </div>
                 <div>
                     <img id="googleLogo" src={GooglePlayLogo} alt="GooglePlay Logo" />
                     <img src={AppleLogo} alt="Apple Logo" />
                 </div>
 
-                <div id="AMeditationText">
-                    <style type="text/css">
-                        {`
-                        h1 {
-                            font-size: 64px;
-                            font-weight: bold;
-                            }
-                        `}
-                    </style>
-                    <h1>A meditation experience
+                <div className="AMeditationTextDiv">
+                    
+                    <h1 id = "AMeditationText">A meditation experience
                     <br></br>
                     with crypto rewards.
                     </h1>
                 </div>
 
-                <div id="buttonSpacing">
+                <div className="whitePaperButtonSpacing">
                     <style type="text/css">
                         {`
-
-                    .btn-huge {
-                        padding: 2rem 5rem;
-                        font-size: 1.5rem;
-                        }
+                        .btn-huge {
+                            padding: 2rem 5rem;
+                            font-size: 1.5rem;
+                          }
                     `}
                     </style>
                     <Button id="buttonStyling" variant="primary" className="rounded-pill shadow-box-example z-depth-5 btn-huge" onClick={() => { window.open("https://drive.google.com/file/d/1AfbMaAE5oNiRPjyab-ao9xft6JZCCDdu/view?usp=sharing") }}> Read our white paper </Button>
