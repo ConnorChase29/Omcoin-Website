9fc4870ab6eac9fe853b6e026ddee4cf4d47dac8
Pharoah Campbell <pharoahcampbell@gmail.com>
2021-03-09 11:34:20 -0500

    refactored landing slide 3

--- src/LandingSlide3.tsx
+++ src/LandingSlide3.tsx
@@ -9,8 +9,8 @@ export class LandingSlide3 extends React.Component<{}, {}>
         return (
             <div id = "Features" className = "Landing3">
                 <div className="Slide3Text">
-                    <h1>Earning crypto has never</h1>
-                    <h1>felt this <span className="blueText">rewarding.</span></h1>
+                    <h1 className = "TitleText">Earning crypto has never</h1>
+                    <h1 className = "TitleText">felt this <span className="blueText">rewarding.</span></h1>
                 </div>
                 <img className = "responsiveImage" src = {Feature1} alt = "Feature1" />
             </div>
