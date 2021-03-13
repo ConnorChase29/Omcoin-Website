8949955927b274f3f8dd702a3ce0cc74438f471b
Connor Chase <connor@Connors-MacBook-Pro.local>
2021-03-13 10:56:54 -0500

    Omcoin Website updates

--- src/LandingSlide3.tsx
+++ src/LandingSlide3.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
 import Feature1 from './Assets/Future-cardRE.png';
+import Feature2 from './Assets/Feature-card-2RE.png';
+import Feature3 from './Assets/Feature-card-3RE.png';
 //import './CSS/Features.css';
 
 
@@ -9,10 +12,22 @@ export class LandingSlide3 extends React.Component<{}, {}>
         return (
             <div id = "Features" className = "Landing3">
                 <div className="Slide3Text">
-                    <h1 className = "TitleText">Earning crypto has never</h1>
-                    <h1 className = "TitleText">felt this <span className="blueText">rewarding.</span></h1>
+                    <h1 className = "TitleText">Earning crypto is</h1>
+                    <h1 className = "TitleText">this <span className="blueText">rewarding.</span></h1>
                 </div>
-                <img className = "responsiveImage" src = {Feature1} alt = "Feature1" />
+                <div className="responsiveImage">
+                    <Carousel>
+                        <Carousel.Item>
+                            <img className="responsiveImage" src={Feature1} alt="Feature 1" />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img className="responsiveImage" src={Feature2} alt="Feature 2" />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img className="responsiveImage" src={Feature3} alt="Feature 3" />
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
             </div>
 
         )
